111de21ccfbc1848041c5ec6b5ce9d4f319281e3
ThiaguDevLyra <thiagu.mangara@gmail.com>
2026-05-08 00:04:25 -0300

    [Lyra AIX] Build Attempt #1

--- src/com/lyra/example/ExampleExtension.java
+++ src/com/lyra/example/ExampleExtension.java
@@ -1,15 +1,17 @@
 package com.lyra.example;
 
-import com.google.appinventor.components.annotations.*;
+import com.google.appinventor.components.annotations.DesignerComponent;
+import com.google.appinventor.components.annotations.SimpleFunction;
+import com.google.appinventor.components.annotations.SimpleObject;
 import com.google.appinventor.components.common.ComponentCategory;
-import com.google.appinventor.components.runtime.*;
-import com.google.appinventor.components.runtime.util.*;
+import com.google.appinventor.components.runtime.AndroidNonvisibleComponent;
+import com.google.appinventor.components.runtime.ComponentContainer;
 
 @DesignerComponent(version = 1,
-                   description = "Fixed extension after compilation error analysis.",
-                   category = ComponentCategory.EXTENSION,
-                   nonVisible = true,
-                   iconName = "images/extension.png")
+    description = "A simple extension to demonstrate fixed build process",
+    category = ComponentCategory.EXTENSION,
+    nonVisible = true,
+    iconName = "images/extension.png")
 @SimpleObject(external = true)
 public class ExampleExtension extends AndroidNonvisibleComponent {
 
@@ -17,8 +19,8 @@ public class ExampleExtension extends AndroidNonvisibleComponent {
         super(container.$form());
     }
 
-    @SimpleFunction(description = "Returns a success message.")
-    public String Status() {
-        return "Extension compiled and running successfully!";
+    @SimpleFunction(description = "Returns a greeting message")
+    public String Greet(String name) {
+        return "Hello, " + name + "! Lyra is here.";
     }
 }
