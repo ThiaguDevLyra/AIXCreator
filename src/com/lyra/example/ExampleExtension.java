6ec707c574082496454b456310ce852ab8cc1e43
ThiaguDevLyra <thiagu.mangara@gmail.com>
2026-05-08 00:12:09 -0300

    [Lyra AIX] Build Attempt #1

--- src/com/lyra/example/ExampleExtension.java
+++ src/com/lyra/example/ExampleExtension.java
@@ -1,26 +1,22 @@
 package com.lyra.example;
 
-import com.google.appinventor.components.annotations.DesignerComponent;
-import com.google.appinventor.components.annotations.SimpleFunction;
-import com.google.appinventor.components.annotations.SimpleObject;
+import com.google.appinventor.components.annotations.*;
 import com.google.appinventor.components.common.ComponentCategory;
-import com.google.appinventor.components.runtime.AndroidNonvisibleComponent;
-import com.google.appinventor.components.runtime.ComponentContainer;
+import com.google.appinventor.components.runtime.*;
 
 @DesignerComponent(version = 1,
-    description = "A simple extension to demonstrate fixed build process",
+    description = "Corrected extension by Lyra Intelligence",
     category = ComponentCategory.EXTENSION,
     nonVisible = true,
     iconName = "images/extension.png")
 @SimpleObject(external = true)
 public class ExampleExtension extends AndroidNonvisibleComponent {
-
     public ExampleExtension(ComponentContainer container) {
         super(container.$form());
     }
 
-    @SimpleFunction(description = "Returns a greeting message")
-    public String Greet(String name) {
-        return "Hello, " + name + "! Lyra is here.";
+    @SimpleFunction(description = "Returns a validation message")
+    public String Status() {
+        return "Extension compiled successfully with Java 8 and Apache Ant.";
     }
 }
